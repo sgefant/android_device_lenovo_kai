3190790534e5e3d29849a2cf4abbddf23ce73ec7
PJBrs <pjbrs@floorenpj.xs4all.nl>
2015-01-06 00:34:02 +0100

    Made some headway with setup-makefiles.sh Does half of what I want it to and the other half shouldn't be so hard any more.

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -10,7 +10,6 @@ DEVICE=kai
 VENDORS="broadcom invensense nvidia widevine"
 
 
-# Function: Interchange file
 for FILE in `cat proprietary-files.txt`; do
   case $FILE in
     "#"*)
@@ -23,36 +22,47 @@ for FILE in `cat proprietary-files.txt`; do
       done
       ;;
     *"/"*)
-      echo Uncommented: $(basename $FILE)
-
-      EXISTING="false"
-      echo Existing is $EXISTING
-      while [[ $EXISTING == "false" ]]; do
-        for i in $( cat $OUTDIR/device-partial.mk | grep -v \# | awk '{print $1}' ); do
-          if [[ $(basename $FILE) == "$i" ]] ; then
-            EXISTING="true"
-            ls $OUTDIR/proprietary/$(basename $FILE)
-            echo cp lenovo-kai-proprietary/$FILE $OUTDIR/proprietary/$(basename $FILE)
-            echo Existing is $EXISTING
-            EXISTING="false"
-          fi
-        done
-        break
-      done
-     
-
-#        cp lenovo-kai-proprietary/$FILE  OUTDIR/proprietary/$FILE
-
-      else # Still not commented out, but not in dir; file is additional
-#        LINEEND=" \\"
-#        COUNT=`wc -l proprietary-files.txt | awk {'print $1'}`
-#        for FILE in `cat proprietary-files.txt`; do
-#          COUNT=`expr $COUNT - 1`
-#          if [ $COUNT = "0" ]; then
-#            LINEEND=""
-#          fi
-          echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" 
-#        done
+      if [ -e $OUTDIR/proprietary/$(basename $FILE) ] ; then
+        echo cp lenovo-kai-proprietary/$FILE $OUTDIR/proprietary/$(basename $FILE)
+      elif [ ! -e $OUTDIR/proprietary/$(basename $FILE) ] ; then
+         # The first time we encounter a new file:
+         # make a new device-partial.mk file. First cat the licence.
+         # Then, cat the existing device-partial.mk. For each line,
+         # awk the filename and 
+         # IF IT SHOULD NOT BE REMOVED
+         # cat it with a trailing backslash
+         # to the new file.
+         # For every time we encounter an addition (for this vendor)
+         # we then add the additional filename like superhansi did:
+                 LINEEND=" \\"
+         #        COUNT=`wc -l proprietary-files.txt | awk {'print $1'}`
+         #        for FILE in `cat proprietary-files.txt`; do
+         #          COUNT=`expr $COUNT - 1`
+         #          if [ $COUNT = "0" ]; then
+         #            LINEEND=""
+         #          fi
+                   echo "    $(basename $FILE)$LINEEND" \>\> $OUTDIR/device-partial.mk
+         #        done
+         # However, we only echo $(basename file). Also, with echo, rev, and awk we split
+         # the suffix from the name so that we can also write an addition to 
+         # $OUTDIR/proprietary/Android.mk
+         # With regard to the latter; this file always ends with an endif statement. We use wordcount
+         # to count the number of lines and the head the file with -n (number of lines -1 ) to a 
+         # new file. After that, we add module descriptions to the makefile, and then 
+         # at the end we echo back in the endif statement.
+         # Module structure (note the trailing line of whitespace:
+         # include $(CLEAR_VARS)
+         # LOCAL_MODULE := libdrmdecrypt
+         # LOCAL_SRC_FILES := libdrmdecrypt.so
+         # LOCAL_MODULE_SUFFIX := .so
+         # LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+         # LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+         # LOCAL_MODULE_TAGS := optional
+         # LOCAL_MODULE_OWNER := widevine
+         # include $(BUILD_PREBUILT)
+         #
+         # LOCAL_MODULE_CLASS can be EXECUTABLES (no module suffix), SHARED_LIBRARIES (filename ends .so) or ETC (any other)
+      fi
       ;;
   esac
 
