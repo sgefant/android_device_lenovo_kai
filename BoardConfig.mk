da756ebb84819e58d9867e04650c8163c51a8647
PJBrs <pjbrs@floorenpj.xs4all.nl>
2015-01-25 21:54:51 +0100

    First try at adding twrp recovery. Build succeeds, doesn't boot

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,18 +6,24 @@ RECOVERY_FSTAB_VERSION := 2
 -include vendor/lenovo/kai/BoardConfigVendor.mk
 include device/lenovo/kai/BoardConfigCommon.mk
 
-TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
+TARGET_BOARD_PLATFORM := tegra3
+TARGET_TEGRA_VERSION := t30
+ 
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := cortex-a7
 ARCH_ARM_HAVE_TLS_REGISTER := true
+ARCH_ARM_USE_NON_NEON_MEMCPY := true
 
+TARGET_NO_RADIOIMAGE := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_BOOTLOADER_BOARD_NAME := a2109a
 
-#BOARD_KERNEL_CMDLINE := tegraid=30.1.3.0.0 mem=1022M@2048M commchip_id=0 androidboot.countrycode=UK androidboot.serialno=FD60295900 androidboot.commchip_id=0 video=tegrafb no_console_suspend=1 console=null,115200n8 debug_uartport=lsport,3 usbcore.old_scheme_first=1 lp0_vec=8192@0xbddf9000 tegra_fbmem=8197120@0xabc01000 core_edp_mv=0 displayboard=0x802a:0x50a0:0x35:0x80:0x00 board_info=f41:a00:1:44:2 tegraboot=sdmmc gpt gpt_sector=30535679 androidboot.hw_ramcode=0 androidboot.board_strap=1 androidboot.bootloader=20121126.15:56 android.kerneltype=normal
 BOARD_KERNEL_CMDLINE := tegraid=30.1.3.0.0 mem=1022M@2048M commchip_id=0 androidboot.commchip_id=0 video=tegrafb no_console_suspend=1 console=null,115200n8 debug_uartport=lsport,3 usbcore.old_scheme_first=1 lp0_vec=8192@0xbddf9000 tegra_fbmem=8197120@0xabc01000 core_edp_mv=0 displayboard=0x802a:0x50a0:0x35:0x80:0x00 board_info=f41:a00:1:44:2 tegraboot=sdmmc gpt gpt_sector=30535679 androidboot.hw_ramcode=0 androidboot.board_strap=1
+#BOARD_KERNEL_CMDLINE := tegraid=30.1.3.0.0 mem=1022M@2048M commchip_id=0 androidboot.commchip_id=0 no_console_suspend=1 console=null,115200n8 debug_uartport=lsport,3 usbcore.old_scheme_first=1 lp0_vec=8192@0xbddf9000 tegra_fbmem=8197120@0xabc01000 core_edp_mv=0 displayboard=0x802a:0x50a0:0x35:0x80:0x00 board_info=f41:a00:1:44:2 tegraboot=sdmmc gpt gpt_sector=30535679 androidboot.hw_ramcode=0 androidboot.board_strap=1
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 
@@ -33,3 +39,30 @@ NEED_WORKAROUND_CORTEX_A9_745320 := true
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_RECOVERY_SWIPE := true
 # TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+# TWRP
+DEVICE_RESOLUTION := 1280x800
+RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := false
+
+TW_INTERNAL_STORAGE_PATH := "/data/media"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/external_sdcard"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sdcard"
+
+#RECOVERY_GRAPHICS_USE_LINELENGTH := false
+#TW_INCLUDE_DUMLOCK := false
+#TW_NO_BATT_PERCENT := false
+#TW_CUSTOM_POWER_BUTTON := 107
+#TW_NO_REBOOT_BOOTLOADER := false
+#TW_NO_REBOOT_RECOVERY := false
+#TW_NO_USB_STORAGE := false
+#RECOVERY_TOUCHSCREEN_SWAP_XY := false
+#RECOVERY_TOUCHSCREEN_FLIP_Y := false
+#RECOVERY_TOUCHSCREEN_FLIP_X := false
+#TW_ALWAYS_RMRF := false
+#TW_NEVER_UNMOUNT_SYSTEM := false
+#TW_INCLUDE_INJECTTWRP := false
+#TW_DEFAULT_EXTERNAL_STORAGE := false
+#TWRP_EVENT_LOGGING := true
+#BOARD_HAS_FLIPPED_SCREEN := false
