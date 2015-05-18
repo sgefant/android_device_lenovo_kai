ffc30769b40a2cee93736d111dbf6cf5d326b5c9
PJBrs <pjbrs@floorenpj.xs4all.nl>
2015-05-18 21:46:01 +0200

    Apparently /system/etc/bluetooth/main.conf is no longer needed
    Change-Id: Ic16cc3386d48f015a5dfeff2f58c77b0462461e8

--- device_kai.mk
+++ device_kai.mk
@@ -98,9 +98,8 @@ PRODUCT_PACKAGES += \
 # we have enough storage space to hold precise GC data
 PRODUCT_TAGS += dalvik.gc.type-precise
 
-# Broadcom (Bluetooth, WiFi)
+# Bluetooth: btmacwriter
 PRODUCT_COPY_FILES += \
-    system/bluetooth/data/main.nonsmartphone.le.conf:system/etc/bluetooth/main.conf \
     device/lenovo/kai/blobs/btmacwriter:system/bin/btmacwriter
 
 # Stupid hack because of missing reference in grouper makefile
