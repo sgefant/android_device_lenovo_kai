4096143497164a6b1a1edf9c491189019ada7430
PJBrs <pjbrs@floorenpj.xs4all.nl>
2016-04-28 23:33:39 +0200

    Fix makefiles for aosp

--- aosp_kai.mk
+++ aosp_kai.mk
@@ -2,3 +2,5 @@ $(call inherit-product, device/lenovo/kai/full_kai.mk)
 
 PRODUCT_NAME := aosp_kai
 
+# Don't restrict vendor folder
+PRODUCT_RESTRICT_VENDOR_FILES := false
--- full_kai.mk
+++ full_kai.mk
@@ -23,10 +23,11 @@
 $(call inherit-product, device/lenovo/kai/device_kai.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_kai
 PRODUCT_DEVICE := kai
 PRODUCT_BRAND := lenovo
 PRODUCT_MODEL := A2109A
+
